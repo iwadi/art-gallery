8d4ad348f28847ac0ce3e545f6a021ea279a6c5f
iwadi <iwadi855@gmail.com>
2025-10-04 11:49:50 +0400

    adding animation to cards

--- src/main/components/Sections/Section_2/Section_2.tsx
+++ src/main/components/Sections/Section_2/Section_2.tsx
@@ -1,3 +1,46 @@
+// import { type GalleryItem } from '../../../../data/mock';
+
+// interface Section2Props {
+//   data: GalleryItem[];
+// }
+
+// function Section_2({ data }: Section2Props) {
+//   const handleImageError = (e: React.SyntheticEvent<HTMLImageElement>) => {
+//     const target = e.target as HTMLImageElement;
+//     target.src = 'src/photos/gallery/404.jpg';
+//     target.alt = 'Image not available';
+//   };
+
+//   return (
+//     <section className="section_2">
+//       <div className="container">
+//         <ul className="section_2__gallery-list">
+//           {data.map(item => (
+//             <li key={item.id} className="section_2__gallery-item">
+//               <div className="section_2__gallery-photo">
+//                 <img src={item.image} alt={item.title} onError={handleImageError} loading="lazy" />
+//               </div>
+//               <div className="section_2__item-info">
+//                 <div className="item-info__element">
+//                   <h2 className="item-title">{item.title}</h2>
+//                   <p className="item-date">{item.date}</p>
+//                   {/* {item.author && (
+//                         <p className="item-author">{item.author}</p>
+//                     )} */}
+//                 </div>
+//               </div>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </section>
+//   );
+// }
+
+// export default Section_2;
+
+
+// Section_2.tsx
 import { type GalleryItem } from '../../../../data/mock';
 
 interface Section2Props {
@@ -18,15 +61,31 @@ function Section_2({ data }: Section2Props) {
           {data.map(item => (
             <li key={item.id} className="section_2__gallery-item">
               <div className="section_2__gallery-photo">
-                <img src={item.image} alt={item.title} onError={handleImageError} loading="lazy" />
+                <img 
+                  src={item.image} 
+                  alt={item.title} 
+                  onError={handleImageError} 
+                  loading="lazy" 
+                />
               </div>
               <div className="section_2__item-info">
                 <div className="item-info__element">
-                  <h2 className="item-title">{item.title}</h2>
-                  <p className="item-date">{item.date}</p>
+                  <div className="item-text name">
+                    <h2 className="item-title">{item.title}</h2>
+                    <p className="item-date">{item.date}</p>
+                  </div>
+                </div>
+                <div className="item-info__additional">
                   {/* {item.author && (
-                        <p className="item-author">{item.author}</p>
-                    )} */}
+                    <p className="item-author">{item.author}</p>
+                  )}
+                  {item.location && (
+                    <p className="item-location">{item.location}</p>
+                  )} */}
+                  <div className="item-text author">
+                    <p className="item-author">Олег</p>
+                    <p className="item-location">Москва</p>
+                  </div>
                 </div>
               </div>
             </li>
